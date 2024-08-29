d0e5acf21fb35c3317b2492bcefe2c8ef630ba01
Fabs <gianini@konova.ch>
2024-08-29 17:59:56 +0200

    Update script

--- read_temp.py
+++ read_temp.py
@@ -1,5 +1,28 @@
 from sense_hat import SenseHat
+import os
+import json
+import uuid
 
+
+file_name = 'uid.json'
+
+if not os.path.exists(file_name):
+    # Generate a new UUID
+    new_uuid = str(uuid.uuid4())
+
+    # Create a dictionary with the UUID
+    data = {
+        'uuid': new_uuid
+    }
+
+    # Write the dictionary to the file in JSON format
+    with open(file_name, 'w') as file:
+        json.dump(data, file, indent=4)
+
+    print(f"File '{file_name}' created with UUID: {new_uuid}")
+else:
+    print(f"File '{file_name}' already exists.")
+    
 sense = SenseHat()
 temperature = sense.get_temperature()
 print(temperature)
