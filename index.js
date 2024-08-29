6c72509cd584fa2d2e9bfc9b948e1c2b78204f4a
Fabs <gianini@konova.ch>
2024-08-29 17:46:18 +0200

    Add script

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
-fetch('http://localhost:1234/session', {
-    method: 'POST'
-})
-    .then(res => res.json())
-    .then(data => {
-        console.log(data)
-    });
+const sense = require('sense-hat-led');
+const SenseHat = require('sense-hat');
+
+// Create an instance of the SenseHat class
+const senseHat = new SenseHat();
+
+// Function to read sensor data
+const readSensorData = async () => {
+    try {
+        const temperature = await senseHat.getTemperature();
+        const humidity = await senseHat.getHumidity();
+        const pressure = await senseHat.getPressure();
+
+        console.log(`Temperature: ${temperature} °C`);
+        console.log(`Humidity: ${humidity} %`);
+        console.log(`Pressure: ${pressure} hPa`);
+    } catch (error) {
+        console.error('Error reading sensor data:', error);
+    }
+};
+
+// Continuously read and display sensor data every 5 seconds
+setInterval(readSensorData, 5000);
